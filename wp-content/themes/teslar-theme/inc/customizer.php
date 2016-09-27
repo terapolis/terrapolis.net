4d410a77171fb434035ea5574a5bbdf341746163
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-09-28 00:43:15 +0300

    add teslar theme by :octopus: http://underscores.me

---
+++ wp-content/themes/teslar-theme/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Teslar Theme Theme Customizer.
+ *
+ * @package Teslar_Theme
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function teslar_theme_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'teslar_theme_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function teslar_theme_customize_preview_js() {
+	wp_enqueue_script( 'teslar_theme_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'teslar_theme_customize_preview_js' );
